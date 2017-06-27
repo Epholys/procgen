de63707f484340ed4d46ebc7843b693710dbb5a2
Epholys <epholys@gmail.com>
2017-06-27 11:43:37 +0000

    Replaces 'std::string' by 'std::vector<char>': strings are for textual data. In our case, we need a container of characters. (See CppCoreGuidelines SL.con.2)

--- LSystem.cpp
+++ LSystem.cpp
@@ -1,39 +1,79 @@
 #include "LSystem.h"
 
-LSystem::LSystem(const std::string& ax, const production_rules& prod)
+
+LSystem::LSystem(const std::vector<char>& ax, const production_rules& prod)
     : axiom{ax},
       rules{prod},
       current{ax}
 {
 }
-
-void LSystem::reset()
+LSystem::LSystem(const std::string& ax, const pretty_production_rules& prod)
+    : axiom{std::vector<char>(ax.begin(), ax.end())},
+      rules{},
+      current{axiom}
 {
-    current = axiom;
+    for (const auto& rule: prod) {
+        rules[rule.first] = std::vector<char> (rule.second.begin(), rule.second.end());
+    }
 }
 
-std::string LSystem::get_result() const
+std::vector<char> LSystem::get_axiom() const
+{
+    return axiom;
+}
+
+// void LSystem::reset()
+// {
+//     current = axiom;
+// }
+
+
+// void LSystem::set_axiom(const std::vector<char>& str)
+// {
+//     axiom = str;
+//     reset(); // The current result must be coherent with the axiom:
+//              // reset the current string to the new axiom
+// }
+
+// LSystem::production_rules LSystem::get_rules() const
+// {
+//     return rules;
+// }
+// void LSystem::set_rules(const production_rules& prod)
+// {
+//     rules = prod;
+//     reset(); // The current result must be coherent with the rule:
+//              // reset the new string to the axiom
+// }
+
+
+std::vector<char> LSystem::get_result() const
 {
     return current;
 }
 
-std::string LSystem::iter(unsigned int n_iter)
+// TODO: Remove 'unsigned int', add Expects().
+// Note: May throw in case of allocation problem.
+std::vector<char> LSystem::iter(unsigned int n_iter)
 {
-    // Use a temporary string: we can't iterate "in place".  
-    std::string result = current;
-    std::string tmp;
+    // Use a temporary vector: we can't iterate "in place".
+    std::vector<char> result = current;
+    std::vector<char> tmp;
         
     for (unsigned int i=0; i<n_iter; ++i) {
         tmp.clear();
             
         for (auto c : result) {
             if(rules.count(c) > 0) {
-                tmp.append(rules.at(c)); // Replace the symbol
-                                         // according to its rule.
+                std::vector<char> rule = rules.at(c);
+
+                // Replace the symbol according to its rule.
+                tmp.insert(tmp.end(), rule.begin(), rule.end());
+
             }
             else {
-                tmp.push_back(c);        // The symbol is a terminal:
-                                         // replace it by itself.
+                // The symbol is a terminal: replace it by itself.
+                tmp.push_back(c);
             }
         }
 
@@ -44,22 +84,9 @@ std::string LSystem::iter(unsigned int n_iter)
     return current;
 }
 
-std::string LSystem::get_axiom() const
+std::ostream& operator<< (std::ostream& stream, std::vector<char> vec)
 {
-    return axiom;
-}
-void LSystem::set_axiom(const std::string& str)
-{
-    reset(); // The current result must be coherent with the axiom. 
-    axiom = str;
-}
-
-LSystem::production_rules LSystem::get_rules() const
-{
-    return rules;
-}
-void LSystem::set_rules(const production_rules& prod)
-{
-    reset(); // The current result must be coherent with the rules. 
-    rules = prod;
+    std::string str (vec.begin(), vec.end());
+    stream << str;
+    return stream;
 }
--- LSystem.h
+++ LSystem.h
@@ -2,61 +2,73 @@
 #define L_SYSTEM_H
 
 
+#include <vector>
 #include <string>
 #include <unordered_map>
+#include <iostream>
 
 // Simple L-system generation class Starting from an axiom and simple
-// production rules, generate by iteration a result string. Simple
-// variant: context-free (one generating symbol by rule) and
+// production rules, generate by iteration a result array of
+// character. This class is a simple variant of more general
+// L-systems: context-free (one generating symbol by rule) and
 // deterministic (at most one rule for each symbol).
+
 class LSystem
 {
 public:
     // A rule is a sole symbol associated with an array of symbols.
     // In an iteration, each symbol will be replaced by its associated
     // array. Symbols without a rules (terminals) are replaced by
-    // themselves.
-    // The rules are contained in a hashmap for quick access during an
-    // iteration.
-    using production_rules = std::unordered_map<char, std::string>;
+    // themselves.  The rules are contained in a hashmap for quick
+    // access during an iteration.
+    using production_rules = std::unordered_map<char, std::vector<char>>;
+
+    // For ease of use, defines a temporary hashmap with std::string
+    // initalization instead of std::vector.
+    using pretty_production_rules = std::unordered_map<char, std::string>;
         
     // Constructors
     LSystem() = default;
-    LSystem(const std::string& ax, const production_rules& prod);
-        
-    // Getters and setters
-    std::string get_axiom() const;
-    void set_axiom(const std::string& str);
+    LSystem(const std::vector<char>& ax, const production_rules& prod);
 
-    production_rules get_rules() const;
-    void set_rules(const production_rules& prod);
+    // Prettier way to create a LSystem:
+    //     { "F" , { 'F', "F+F" } }
+    //  VS
+    //     { {'F'}, { 'F', { 'F', '+', 'F' } } }
+    LSystem(const std::string& ax, const pretty_production_rules& prod);
 
-    // Resets the current string.
-    // Called if the axiom or the production rules are modified.
-    void reset();
+    // // Getters and setters
+    std::vector<char> get_axiom() const;
+    // void set_axiom(const std::vector<char>& str);
 
-    // Get the current string.
-    std::string get_result() const;
+    // production_rules get_rules() const;
+    // void set_rules(const production_rules& prod);
+    // void add_rule(const rule& r);
+    // void remove_rule(char c);
+
+    // // Resets the current string.
+    // // Called if the axiom or the production rules are modified.
+    // void reset();
+
+    // Get the current result.
+    std::vector<char> get_result() const;
     
-    // Iterate n_iter times and return the updated current string.
-    std::string iter(unsigned int n_iter = 1);
-
-    // May be used in the future:
-    // // void add_rule(const rule& r);
-    // // void remove_rule(char c);
+    // Iterate n_iter times and return the updated current result.
+    std::vector<char> iter(unsigned int n_iter = 1);
        
 private:
     // The starting point defining the initial state.
-    std::string axiom = "";
+    std::vector<char> axiom = {};
 
     // The production rules applied in each iteration.
     production_rules rules = {};
 
-    // The current string.
+    // The current result.
     // Is always coherent with the axiom and the rules.
-    std::string current = "";
+    std::vector<char> current = {};
 };
 
+std::ostream& operator<< (std::ostream& stream, std::vector<char> vec);
 
 #endif
 
