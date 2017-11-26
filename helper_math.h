55170430313a983dd2928cbad7636acb67876fb1
Epholys <epholys@gmail.com>
2017-11-26 13:47:55 +0000

    Add constexpr to helper_math.h

--- helper_math.h
+++ helper_math.h
@@ -4,13 +4,13 @@
 // This namespace defines commonly used function and constants missing in <cmath>.
 namespace math
 {
-    const double pi = 3.14159;
+    constexpr double pi = 3.14159;
 
-    inline double degree_to_rad (double deg) { return deg * pi / 180; }
-    inline double rad_to_degree (double rad) { return rad * 180 / pi; }
+    constexpr double degree_to_rad (double deg) { return deg * pi / 180; }
+    constexpr double rad_to_degree (double rad) { return rad * 180 / pi; }
 
-    inline float degree_to_rad (float deg) { return deg * pi / 180; }
-    inline float rad_to_degree (float rad) { return rad * 180 / pi; }
+    constexpr float degree_to_rad (float deg) { return deg * pi / 180; }
+    constexpr float rad_to_degree (float rad) { return rad * 180 / pi; }
 }
 
 #endif
