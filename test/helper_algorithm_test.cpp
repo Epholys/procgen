b0795677bdc43756e6ce0ff2c28c3472dc2156c6
Epholys <epholys@gmail.com>
2018-02-11 13:18:23 +0000

    Complete the '*duplicate()' helper functions

--- test/helper_algorithm_test.cpp
+++ test/helper_algorithm_test.cpp
@@ -5,7 +5,40 @@
 
 #include "helper_algorithm.h"
 
-TEST(helper_algorithm_test, find_duplicate_simple)
+TEST(helper_algorithm_test, find_existing_val)
+{
+    std::vector<int> v { 1, 2, 3 };
+
+    auto first  = v.begin();
+    auto last   = v.end();
+    auto penultimate = std::prev(last);
+
+    ASSERT_TRUE(find_existing(first, last, 3) == penultimate);
+    ASSERT_TRUE(find_existing(first, penultimate, 3) == penultimate);
+    ASSERT_TRUE(find_existing(first, last, 4) == last);
+}
+
+TEST(helper_algorithm_test, find_existing_val_pred)
+{
+    std::vector<std::string> v { "AA", "BB", "CC" };
+
+    auto predicate =
+        [](const auto& s1, const auto& s2)
+        {
+            return s1.at(0) == std::string(s2).at(0);
+        };
+    
+    auto first  = v.begin();
+    auto last   = v.end();
+    auto penultimate = std::prev(last);
+
+    ASSERT_TRUE(find_existing(first, last, "CDE", predicate) == penultimate);
+    ASSERT_TRUE(find_existing(first, penultimate, "CDE", predicate) == penultimate);
+    ASSERT_TRUE(find_existing(first, last, "DEF", predicate) == last);
+
+}
+
+TEST(helper_algorithm_test, find_duplicate_it)
 {
     std::vector<int> v { 1, 2, 3, 1 };
 
@@ -14,12 +47,12 @@ TEST(helper_algorithm_test, find_duplicate_simple)
     auto last   = v.end();
     auto penultimate = std::prev(last);
 
-    ASSERT_TRUE(find_duplicate(first, first, last) != last);
-    ASSERT_FALSE(find_duplicate(first, first, penultimate) != penultimate);
-    ASSERT_FALSE(find_duplicate(second, first, last) != last);
+    ASSERT_TRUE(find_duplicate(first, first, last) == penultimate);
+    ASSERT_TRUE(find_duplicate(first, first, penultimate) == penultimate);
+    ASSERT_TRUE(find_duplicate(second, first, last) == last);
 }
 
-TEST(helper_algorithm_test, find_duplicate_predicate)
+TEST(helper_algorithm_test, find_duplicate_it_pred)
 {
     std::vector<std::string> v { "AA", "BB", "CC", "AB" };
 
@@ -34,7 +67,7 @@ TEST(helper_algorithm_test, find_duplicate_predicate)
     auto last   = v.end();
     auto penultimate = std::prev(last);
 
-    ASSERT_TRUE(find_duplicate(first, first, last, predicate) != last);
-    ASSERT_FALSE(find_duplicate(first, first, penultimate, predicate) != penultimate);
-    ASSERT_FALSE(find_duplicate(second, first, last, predicate) != last);
+    ASSERT_TRUE(find_duplicate(first, first, last, predicate) == penultimate);
+    ASSERT_TRUE(find_duplicate(first, first, penultimate, predicate) == penultimate);
+    ASSERT_TRUE(find_duplicate(second, first, last, predicate) == last);
 }
