0e42a63c04c23e7a050d03fa0398fe4142113fa5
Epholys <epholys@gmail.com>
2018-03-31 15:23:59 +0000

    Refactor the 'handle_input()' function into a proper class

---
+++ WindowController.cpp
@@ -0,0 +1,86 @@
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
+#include "WindowController.h"
+
+void WindowController::handle_input(sf::RenderWindow &window)
+{
+    sf::View view = window.getView();
+    ImGuiIO& imgui_io = ImGui::GetIO();
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+        // ImGui has the priority as it is the topmost GUI.
+        ImGui::SFML::ProcessEvent(event);
+
+        // Close the Window if necessary
+        if (event.type == sf::Event::Closed ||
+            (event.type == sf::Event::KeyPressed &&
+             event.key.code == sf::Keyboard::Escape))
+        {
+            window.close();
+        }
+
+        else if (event.type == sf::Event::GainedFocus)
+        {
+            has_focus_ = true;
+            // Note: the view can not move yet: the old position of the mouse is
+            // still in memory, it must be updated.
+        }
+        else if (event.type == sf::Event::LostFocus)
+        {
+            has_focus_ = false;
+            view_can_move_ = false;
+        }
+        else if (event.type == sf::Event::Resized)
+        {
+            view.setSize(event.size.width, event.size.height);
+        }
+
+        else if (has_focus_)
+        {
+            if(!imgui_io.WantCaptureMouse &&
+               event.type == sf::Event::MouseWheelMoved)
+            {
+                // Adjust the zoom level
+                auto delta = event.mouseWheel.delta;
+                if (delta>0)
+                {
+                    zoom_level_ *= 0.9f;
+                    view.zoom(0.9f);
+                }
+                else if (delta<0)
+                {
+                    zoom_level_ *= 1.1f;
+                    view.zoom(1.1f);
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed &&
+                event.mouseButton.button == sf::Mouse::Left)
+            {
+                // Update the mouse position and finally signal that the view
+                // can now move.
+                mouse_position_ = sf::Mouse::getPosition(window);
+                view_can_move_ = true;
+            }
+        }
+    }
+
+    // Dragging behaviour
+    if (has_focus_ && view_can_move_ &&
+        !imgui_io.WantCaptureMouse &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        sf::Vector2i new_position = sf::Mouse::getPosition(window);
+        sf::IntRect window_rect (sf::Vector2i(0,0), sf::Vector2i(window.getSize()));
+        if (window_rect.contains(new_position))
+        {
+            sf::Vector2i mouse_delta = mouse_position_ - new_position;
+            view.move(sf::Vector2f(mouse_delta) * zoom_level_);
+            mouse_position_ = new_position;
+        }
+    }
+    
+    window.setView(view);
+}
--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "Turtle.h"
 #include "helper_math.h"
 #include "procgui.h"
+#include "WindowController.h"
 
 #include <functional>
 
@@ -19,24 +20,14 @@ using namespace drawing;
 using namespace math;
 using namespace procgui;
 
-// Forward Declaration
-void handle_input(sf::RenderWindow& window);
-
-// Standard main() for the procgen application
-int main(/*int argc, char* argv[]*/)
+int main()
 {
-    // if (argc < 2)
-    // {
-    //     std::cerr << "Usage: procgen [n]" << std::endl;
-    //     return 1;;
-    // }
-    // int n_iter = std::atoi(argv[1]);
-    // std::cout << "Number of iterations: " << n_iter << std::endl;
-
     sf::RenderWindow window(sf::VideoMode(1600, 900), "Procgen");
     window.setVerticalSyncEnabled(true);
     ImGui::SFML::Init(window);
 
+    WindowController window_controller;
+    
     auto serpinski = std::make_shared<LSystem>(LSystem { "F", { { 'F', "G-F-G" }, { 'G', "F+G+F" } } });
     auto plant = std::make_shared<LSystem>(LSystem { "X", { { 'X', "F[-X][X]F[-X]+FX" }, { 'F', "FF" } } });
     auto map = std::make_shared<InterpretationMap>(InterpretationMap
@@ -67,7 +58,8 @@ int main(/*int argc, char* argv[]*/)
     while (window.isOpen())
     {
         window.clear();
-        handle_input(window);
+
+        window_controller.handle_input(window);
         
         ImGui::SFML::Update(window, delta_clock.restart());
 
@@ -85,84 +77,4 @@ int main(/*int argc, char* argv[]*/)
     ImGui::SFML::Shutdown();
 
     return 0;
-}
-
-// Handle input with SFML and ImGui
-void handle_input(sf::RenderWindow& window)
-{
-    static float zoom_level = 1.f;
-    static sf::Vector2i mouse_position {};
-    static bool has_focus = true;
-    static bool can_move = false;
-    
-    sf::View view = window.getView();
-    ImGuiIO& imgui_io = ImGui::GetIO();
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-        ImGui::SFML::ProcessEvent(event);
-
-        if (event.type == sf::Event::Closed ||
-            (event.type == sf::Event::KeyPressed &&
-             event.key.code == sf::Keyboard::Escape))
-        {
-            window.close();
-        }
-
-        else if (event.type == sf::Event::GainedFocus)
-        {
-            has_focus = true;
-        }
-        else if (event.type == sf::Event::LostFocus)
-        {
-            has_focus = false;
-            can_move = false;
-        }
-        else if (event.type == sf::Event::Resized)
-        {
-            view.setSize(event.size.width, event.size.height);
-        }
-            
-        else if (has_focus) // && !imgui_io.WantCaptureMouse)
-        {
-            if(!imgui_io.WantCaptureMouse &&
-                event.type == sf::Event::MouseWheelMoved)
-            {
-                auto delta = event.mouseWheel.delta;
-                if (delta>0)
-                {
-                    zoom_level *= 0.9f;
-                    view.zoom(0.9f);
-                }
-                else if (delta<0)
-                {
-                    zoom_level *= 1.1f;
-                    view.zoom(1.1f);
-                }
-            }
-
-            if (event.type == sf::Event::MouseButtonPressed &&
-                event.mouseButton.button == sf::Mouse::Left)
-            {
-                mouse_position = sf::Mouse::getPosition(window);
-                can_move = true;
-            }
-        }
-    }
-
-    if (has_focus && can_move &&
-        !imgui_io.WantCaptureMouse &&
-        sf::Mouse::isButtonPressed(sf::Mouse::Left))
-    {
-        sf::Vector2i new_position = sf::Mouse::getPosition(window);
-        sf::IntRect window_rect (sf::Vector2i(0,0), sf::Vector2i(window.getSize()));
-        if (window_rect.contains(new_position))
-        {
-            sf::Vector2i mouse_delta = mouse_position - new_position;
-            view.move(sf::Vector2f(mouse_delta) * zoom_level);
-            mouse_position = new_position;
-        }
-    }
-    
-    window.setView(view);
 }
