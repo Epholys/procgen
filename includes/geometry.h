ff240558b18616307a6e95dab910c53d9afe53cd
Epholys <epholys@gmail.com>
2018-06-03 14:37:27 +0000

    wip: linear gradient with an angle

--- includes/geometry.h
+++ includes/geometry.h
@@ -2,6 +2,7 @@
 #define GEOMETRY_H
 
 
+#include <cmath>
 #include <vector>
 #include <SFML/Graphics.hpp>
 
@@ -23,6 +24,25 @@ namespace geometry
     // code for more informations.
     std::vector<sf::FloatRect> compute_sub_boxes(const std::vector<sf::Vertex>& vertices,
                                                  int max_boxes);
+
+    bool intersection (const sf::Vector2f& a, const sf::Vector2f& u, const sf::Vector2f& b, const sf::Vector2f v, sf::Vector2f& intersection)
+    {
+        float dx = b.x - a.x;
+        float dy = b.y - a.y;
+        float det = v.x * u.y - v.y * u.x;
+        if (det != 0)
+        {
+            float t = (dy * v.x - dx * v.y) / det;
+            intersection = a + t * u;
+            return true;
+        }
+        return false;
+    }
+
+    float distance (const sf::Vector2f& a, const sf::Vector2f& b)
+    {
+        return std::sqrt(std::pow(b.x-a.x, 2)+std::pow(b.y-a.y, 2));
+    }
 }
 
 
--- src/VertexPainter.cpp
+++ src/VertexPainter.cpp
@@ -1,5 +1,6 @@
 #include "VertexPainter.h"
 #include "procgui.h"
+#include "geometry.h"
 
 namespace colors
 {
@@ -19,12 +20,77 @@ namespace colors
         {
             return;
         }
+
+        struct Line
+        {
+            sf::Vector2f point;
+            sf::Vector2f direction;
+        };
+        enum bound { Upper=0, Rightmost, Bottom, Leftmost };
+        std::vector<Line> bounds (4);
+        bounds.at(Upper) = {{bounding_box.left, bounding_box.top}, {1,0}};
+        bounds.at(Rightmost) = {{bounding_box.left+bounding_box.width, bounding_box.top}, {0,1}};
+        bounds.at(Bottom) = {{bounding_box.left+bounding_box.width, bounding_box.top+bounding_box.height}, {-1,0}};
+        bounds.at(Leftmost) = {{bounding_box.left, bounding_box.top+bounding_box.height}, {0,-1}};
+                
+        const float angle = 45.;
+        std::pair<Line, Line> higher_bounds; // at the direction pointed by the angle
+        if (angle >= 0 && angle < 180.)
+        {
+            higher_bounds.first = bounds.at(Upper);
+        }
+        else
+        {
+            higher_bounds.first = bounds.at(Bottom);
+        }
+        if (angle >= 90. && angle < 270.)
+        {
+            higher_bounds.second = bounds.at(Leftmost);
+        }
+        else
+        {
+            higher_bounds.second = bounds.at(Rightmost);
+        }
         
-        float top = bounding_box.top;
-        float bottom = top + bounding_box.height;
+        const sf::Vector2f middle {bounding_box.left+bounding_box.width/2.f,
+                                   bounding_box.top+bounding_box.height/2.f};
+        const sf::Vector2f direction {std::cos(math::degree_to_rad(angle)), std::sin(math::degree_to_rad(angle))};
+
+        sf::Vector2f intersection {middle};
+        geometry::intersection(middle, direction, higher_bounds.first.point, higher_bounds.first.direction, intersection);
+        float distance_from_middle = geometry::distance(middle, intersection);
+        
+        sf::Vector2f intersection2 {middle};
+        geometry::intersection(middle, direction, higher_bounds.second.point, higher_bounds.second.direction, intersection2);
+        float distance_from_middle2 = geometry::distance(middle, intersection2);
+
+        if (distance_from_middle2 < distance_from_middle)
+        {
+            intersection = intersection2;
+        }
+        
+        sf::Vector2f opposite_intersection = middle - (intersection - middle);
+
+        float distance = geometry::distance(opposite_intersection, intersection);
+        
+        sf::Vector2f projection;
+        sf::Vector2f AB = opposite_intersection - intersection;
+        float AB_squared = AB.x*AB.x + AB.y*AB.y;
+        if (AB_squared == 0)
+        {
+            projection = intersection;
+        }
+
         for (auto& v : vertices)
         {
-            auto color = generator_->get((bottom - v.position.y) / (bottom - top));
+            sf::Vector2f Ap = v.position - intersection;
+            float t = (Ap.x*AB.x + Ap.y*AB.y) / AB_squared;
+            projection = intersection + t*AB;
+
+            float lerp = std::sqrt(std::pow(opposite_intersection.x-projection.x,2)+
+                                   std::pow(opposite_intersection.y-projection.y,2)) / distance;
+
+            sf::Color color = generator_->get(lerp);
             color.a = v.color.a;
             v.color = color;
         }
