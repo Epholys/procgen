21506434a0360888d2698dda5ab3b1ed196c584d
Epholys <epholys@gmail.com>
2019-12-14 15:02:25 +0100

    Modify title of the window

--- src/RenderWindow.cpp
+++ src/RenderWindow.cpp
@@ -9,7 +9,7 @@ namespace sfml_window
 
     void init_window()
     {
-        window.create(sf::VideoMode(default_window_size.x, default_window_size.y), "Procgen");
+        window.create(sf::VideoMode(default_window_size.x, default_window_size.y), "LSys");
         window.setVerticalSyncEnabled(true);
 #ifdef _WIN32 // Window starts with wrong scaling, changing it like this stop this bug.
 		window.setSize(sf::Vector2u(default_window_size.x-1, default_window_size.y));
